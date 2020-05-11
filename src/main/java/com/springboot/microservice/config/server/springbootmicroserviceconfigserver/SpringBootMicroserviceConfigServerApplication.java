55d96596fb16c2a73420b310116f8e12d1707423
Anton_Nikitin <anton_nikitin@epam.com>
2020-05-11 22:16:46 +0400

    test

---
+++ src/main/java/com/springboot/microservice/config/server/springbootmicroserviceconfigserver/SpringBootMicroserviceConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.microservice.config.server.springbootmicroserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootMicroserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceConfigServerApplication.class, args);
+	}
+
+}
